c0a91974872defaa5ffa84de397d84948f3b44a6
Charles Schiermeyer <matkatmusic@gmail.com>
2019-06-24 14:54:23 -0700

    added Thread Example

--- Source/ThreadExample.h
+++ Source/ThreadExample.h
@@ -9,3 +9,48 @@
 */
 
 #pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+struct MyThreadExample : Thread
+{
+    MyThreadExample() : Thread("Thread Example")
+    {
+        startThread();
+    }
+    
+    ~MyThreadExample()
+    {
+        stopThread(500);
+    }
+    
+    void run() override
+    {
+        while( true )
+        {
+            if( threadShouldExit() )
+                break;
+            
+            /*
+             do some work
+             */
+            if( threadShouldExit() )
+                break;
+            /*
+             do some work
+             */
+            if( threadShouldExit() )
+                break;
+            
+            wait( 10 );
+            //or
+            wait( -1 );
+            /*
+             MyThread mt;
+             mt.notify();
+             */
+        }
+    }
+};
+
+
